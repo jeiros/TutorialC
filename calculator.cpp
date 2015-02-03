9eadf6b1cdc472bc5d97c6bea199a268d1ac043f
Juan Eiros <juaneiros@hotmail.com>
2015-02-03 16:59:43 +0000

    added some functions

---
+++ calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+
+int main()
+{
+    using namespace std;
+    cout << "Enter a value: ";
+    double dX;
+    cin >> dX;
+ 
+    cout << "Enter a second value: ";
+    double dY;
+    cin >> dY;
+ 
+    cout << "Enter one of the following: +, -, *, or /";
+    char chChoice;
+    cin >> chChoice;
+
+    if (chChoice == "+")
+    {
+    	cout << dX << " + " << dY << " = " << dX + dY << endl;
+    }
+    if (chChoice == "-")
+    {
+    	cout << dX << " - " << dY << " = " << dX - dY << endl;
+    }
+    if (chChoice == "*")
+    {
+    	cout << dX << " * " << dY << " = " << dX * dY << endl;
+    }
+    if (chChoice == "/")
+    {
+    	cout << dX << " / " << dY << " = " << dX / dY << endl;
+    }
+    else
+    	cout << "Please enter a valid symbol";
+    return 0;
+}
