b168171e0586479d370387be149c10b35b4f699f
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-02-09 15:44:35 +0000

    Added a pointer to the real root value and use it in main.

--- week2/exercise2/find_number_of_roots.c
+++ week2/exercise2/find_number_of_roots.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-int find_number_of_roots(int A, int B, int C, int D) {
+int find_number_of_roots(int A, int B, int C, int D, double *real) {
 
   double a_0 = D/A;
   double a_1 = C/A;
@@ -11,16 +11,12 @@ int find_number_of_roots(int A, int B, int C, int D) {
   double q = ((a_2*a_2) - 3*a_1)/9;
   double r = (2*pow(a_2,3) - 9*a_1*a_2 + 27*a_0)/54;
 
-  double r_real;
-
   if (((pow(q,3) - r*r) < 0)) {
-    r_real = (-fabs(r)/r)*((pow(sqrt(r*r-pow(q,3))+fabs(r),1/3)) +
+    *real = (-fabs(r)/r)*((pow(sqrt(r*r-pow(q,3))+fabs(r),1/3)) +
              (q/pow(sqrt(r*r-pow(q,3))+fabs(r),1/3))) - a_2/3;
-    printf("The real root is: %f\n", r_real);
     return 1;
   } else {
-    r_real = (-2*sqrt(q)*cos(acos(r*pow(q,-3/2))/3)) - a_2/3;
-    printf("One of the real roots is: %f\n", r_real);
+    *real = (-2*sqrt(q)*cos(acos(r*pow(q,-3/2))/3)) - a_2/3;
     return 3;
   }
 }
--- week2/exercise2/main.c
+++ week2/exercise2/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int find_number_of_roots(int A, int B, int C, int D);
+int find_number_of_roots(int A, int B, int C, int D, double *real);
 
 int main(int argc, char const *argv[]) {
   double A, B, C, D;
-
+  double real;
   printf("Enter coefficients:");
   scanf("%lf %lf %lf %lf", &A, &B, &C, &D);
 
-  switch (find_number_of_roots(A, B, C, D)) {
+  switch (find_number_of_roots(A, B, C, D, &real)) {
     case 1:
-      printf("One real root\n");
+      printf("One real root and a complex conjugate\n");
+      printf("The real root value is: %f\n", real);
       break;
     case 3:
       printf("Three real roots\n");
+      printf("One of the real roots is:%f\n", real);
       break;
   }
   return 0;
